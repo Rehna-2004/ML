6d3c94c65ce88d58663ca75930a693206653d750
Rehna-2004 <rehnampta@gmail.com>
2026-07-21 15:01:47 +0530

    Add files via upload

---
+++ inverse.py
@@ -0,0 +1,24 @@
+import numpy as np
+n=int(input("Enter the order of the square matrix: "))
+print("Enter the matrix elements row by row:")
+matrix = []
+for i in range(n):
+    row=list(map(float,input().split()))
+    matrix.append(row)
+A=np.array(matrix)
+print("\nMatrix A:")
+print(A)
+det =np.linalg.det(A)
+print("\nDeterminant of matrix :")
+print(det)
+if det != 0:
+        inverse = np.linalg.inv(A)
+        print("\nInverse of matrix :")
+        print(inverse)
+else:
+        print("\nInverse does not exist (Determinant = 0).")
+        eigen_values, eigen_vectors = np.linalg.eig(A)
+        print("\nEigen Values :")
+        print(eigen_values)
+        print("\nEigen Vectors :") 
+        print(eigen_vectors)   
